cabdebda941fc4db3a1341faceaecf39b24de441
BehzadBz <43294940+BehzadBz@users.noreply.github.com>
2025-01-19 17:58:44 +0330

    Finished Interfaces.

---
+++ T5-Classes-&-Interfaces/src/app.ts
@@ -0,0 +1,46 @@
+// type AddFN = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name?: string;
+  age: number;
+
+  constructor(age: number, n?: string) {
+    if (n) {
+      this.name = n;
+    }
+    this.age = age;
+  }
+
+  greet(phrase: string) {
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
+  }
+}
+
+let user1: Greetable;
+
+user1 = new Person(34);
+
+user1.greet("Hi there - I am");
+console.log(user1);
